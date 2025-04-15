1064be81d694bc2b3c308de6b2951c21a3f89677
Tudor Ambarus <tudordana@google.com>
2025-04-15 10:32:51 +0000

    Revert "ANDROID: Add padding to kunit structures exposed on the KMI"
    This reverts commit 5677cab2b2a8016c85c8ca5863517afd7dd7f42e.
    
    It causes build errors with upstream 'rust-fixes-6.15-merge' merge and
    can be reconsidered later if needed.
    
    Bug: 410759397
    Change-Id: Iabbf41b7dd098b9b83d4cd940e4a3c714cb3e125
    Cc: Sid Nayyar <sidnayyar@google.com>
    Signed-off-by: Tudor Ambarus <tudordana@google.com>

--- include/kunit/test.h
+++ include/kunit/test.h
@@ -26,7 +26,6 @@
 #include <linux/spinlock.h>
 #include <linux/string.h>
 #include <linux/types.h>
-#include <linux/android_kabi.h>
 
 #include <asm/rwonce.h>
 #include <asm/sections.h>
@@ -84,8 +83,6 @@ enum kunit_speed {
 /* Holds attributes for each test case and suite */
 struct kunit_attributes {
 	enum kunit_speed speed;
-
-	ANDROID_KABI_RESERVE(1);
 };
 
 /**
@@ -136,8 +133,6 @@ struct kunit_case {
 	enum kunit_status status;
 	char *module_name;
 	struct string_stream *log;
-
-	ANDROID_KABI_RESERVE(1);
 };
 
 static inline char *kunit_status_to_ok_not_ok(enum kunit_status status)
@@ -260,8 +255,6 @@ struct kunit_suite {
 	struct string_stream *log;
 	int suite_init_err;
 	bool is_init;
-
-	ANDROID_KABI_RESERVE(1);
 };
 
 /* Stores an array of suites, end points one past the end */
@@ -311,8 +304,6 @@ struct kunit {
 	char status_comment[KUNIT_STATUS_COMMENT_SIZE];
 	/* Saves the last seen test. Useful to help with faults. */
 	struct kunit_loc last_seen;
-
-	ANDROID_KABI_RESERVE(1);
 };
 
 static inline void kunit_set_failure(struct kunit *test)
